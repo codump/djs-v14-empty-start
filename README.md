f2b8bd0f477405bc369f54c62f4cac4470d974f0
kip <info@brotherops.com>
2026-01-09 14:14:50 +0100

    Initialize Discord.js v14 bot starter project
    Add base project structure for a Discord.js v14 bot, including command and event handling, a sample ping command, deployment script, configuration template, and updated README. Project dependencies and ignore rules are also set up.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # djs-v14-empty-start
-Start for discord bot.
+Start for discord bot. From the d.js guide with a minor edit to commands, there are no subfolders. Instructions can be found in the guide. You don't have to code, only follow the setup instructions. Also change `empty-config.json` to `config.json` and fill in the details.
+
+https://discordjs.guide/legacy/preparations/app-setup
---
+++ deploy-commands.js
@@ -0,0 +1,31 @@
+const { REST, Routes } = require('discord.js')
+const { botClientId, botToken } = require('./config.json')
+const fs = require('node:fs')
+
+const commands = []
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'))
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`)
+	commands.push(command.data.toJSON())
+}
+
+const rest = new REST({ version: '10' }).setToken(botToken)
+(async () => {
+	try {
+		console.log(`Started refreshing ${commands.length} application (/) commands.`)
+
+		/* For guild commands use the snippet below and add guildId to config.json
+    const data = await rest.put(
+			Routes.applicationGuildCommands(botClientId, guildId),
+			{ body: commands },
+		)
+    */
+		const data = await rest.put(
+			Routes.applicationCommands(botClientId),
+			{ body: commands },
+		)
+		console.log(`Successfully reloaded ${data.length} application (/) commands.`)
+	} catch (error) {
+		console.error(error)
+	}
+})()
