c4241f9bce14e445edf66c133a49a10b77d02719
kip <info@brotherops.com>
2026-04-19 21:28:18 +0200

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -9,7 +9,7 @@ for (const file of commandFiles) {
 	commands.push(command.data.toJSON())
 }
 
-const rest = new REST({ version: '10' }).setToken(botToken)
+const rest = new REST({ version: '10' }).setToken(botToken);
 (async () => {
 	try {
 		console.log(`Started refreshing ${commands.length} application (/) commands.`)
